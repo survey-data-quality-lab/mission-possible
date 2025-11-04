1d7243b14e1cf0db08f5f0a04f0cfef72efef353
can-celebi <57803134+can-celebi@users.noreply.github.com>
2025-11-04 17:15:32 +0100

    Update keylog.js

--- trackers/keylog/keylog.js
+++ trackers/keylog/keylog.js
@@ -2,24 +2,20 @@ Qualtrics.SurveyEngine.addOnload(function () {
     
     var keylog = [];
 
-
     // Keystroke logging
     document.addEventListener("keydown", function (e) {
         const event = { key: e.key, time: Date.now() };
         keylog.push(event);
     });
 
-
     // Detect and log large input jumps
     const inputField = document.querySelector("textarea");
     let lastLen = inputField ? inputField.value.length : 0;
-
 
     if (inputField) {
         inputField.addEventListener("input", function () {
             const len = inputField.value.length;
             const jump = len - lastLen;
-
 
             if (jump > 10) {
                 keylog.push({
@@ -29,18 +25,13 @@ Qualtrics.SurveyEngine.addOnload(function () {
                     total: len
                 });
             }
-
-
             lastLen = len;
         });
     }
 
+// Save everything when page submits
+Qualtrics.SurveyEngine.addOnPageSubmit(function () {
 
-    // Save everything when page submits
-    Qualtrics.SurveyEngine.addOnPageSubmit(function () {
-
-
-        // Set Embedded Data fields
-        Qualtrics.SurveyEngine.setEmbeddedData("key_log", JSON.stringify(keylog));
+   Qualtrics.SurveyEngine.setEmbeddedData("key_log", JSON.stringify(keylog));
     });
 });
